ef831347fe63553ec8dfafef435b60efbcf6f72d
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-05-02 23:14:02 +0300

    refactor boxdemo class add one more box object

--- BoxDemo.java
+++ BoxDemo.java
@@ -1,11 +1,17 @@
 public class BoxDemo {
     public static void main(String[] args) {
-        Box mybox=new Box();
+        Box mybox1=new Box();
+        Box mybox2=new Box();
         double vol;
-        mybox.wigth=10;
-        mybox.height=20;
-        mybox.depth=15;
-        vol= mybox.wigth* mybox.height* mybox.depth;
+        mybox1.wigth=10;
+        mybox1.height=20;
+        mybox1.depth=15;
+        mybox2.wigth=3;
+        mybox2.height=6;
+        mybox2.depth=9;
+        vol= mybox1.wigth* mybox1.height* mybox1.depth;
+        System.out.println("Объем равен " + vol);
+        vol= mybox2.wigth* mybox2.height*mybox2.depth;
         System.out.println("Объем равен " + vol);
     }
 }
