1093446a7f789acdba406b50a730050a8ccaf8d8
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-05-02 23:44:52 +0300

    refactor boxdemo class use constructor with params

--- Box.java
+++ Box.java
@@ -3,7 +3,6 @@ public class Box {
     double height;
     double depth;
     Box(double w, double h, double d){
-        System.out.println("Конструирование объекта Box");
         wigth=w;
         height=h;
         depth=d;
