521552919c6835e49a526bd082411d423d404b25
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-05-02 23:24:07 +0300

    refactor box class refactor volume method to return value

--- Box.java
+++ Box.java
@@ -2,8 +2,7 @@ public class Box {
     double wigth;
     double height;
     double depth;
-    void volume(){
-        System.out.print("Объем равен ");
-        System.out.println(wigth * height * depth);
+    double volume(){
+        return  wigth*height*depth;
     }
 }
